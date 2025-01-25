b577d7b9e1b769860c87c3a1dea9d8e372cbee67
moh-shawwa <malshawwa@trustangle.com>
2025-01-25 08:44:58 +0200

    MajorityElement

---
+++ Leetcode-Problems/MajorityElement/MajorityElement.cs
@@ -0,0 +1,26 @@
+﻿using System;
+public class MajorityElementClass
+{
+    public static int MajorityElement(int[] nums)
+    {
+        int value = 0, count = 0;
+
+        foreach (int num in nums)
+        {
+            if (count == 0) value = num;
+            
+            count += (num == value) ? 1 : -1;
+        }
+
+        return value;
+    }
+
+    public static void Main()
+    {
+        int[] nums = { 2, 1, 1, 1 };
+
+        int result = MajorityElement(nums);
+        
+       Console.WriteLine($"Result: {result}");
+    }
+}
