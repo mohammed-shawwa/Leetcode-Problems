1449b9633fe7524172dfba0f0dc479882a77b2e4
moh-shawwa <malshawwa@trustangle.com>
2025-03-05 11:54:13 +0200

    Rotate Array

---
+++ Leetcode-Problems/RotateArray/RotateArray.cs
@@ -0,0 +1,20 @@
+﻿using System;
+public class RotateArray
+{
+    public void Rotate(int[] nums, int k) {
+        k = k % nums.Length;
+        if(nums.Length <= 1 || k == 0) return;
+        reverse(nums, 0, nums.Length - 1 - k);
+        reverse(nums, nums.Length - k, nums.Length - 1);
+        reverse(nums, 0, nums.Length - 1);
+    }
+
+    public void reverse(int[] nums, int start, int end){
+        for (int i = 0; i <= (end - start) / 2; i++)
+            {
+                int tmp = nums[i + start];
+                nums[i + start] = nums[end - i];
+                nums[end - i] = tmp;
+            }
+    }
+}
